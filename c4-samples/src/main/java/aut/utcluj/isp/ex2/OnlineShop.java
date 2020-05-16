ef3ce14aec231cd3fd2742150765b97dac4779aa
Bianca Gavrilescu <Bianca.GAVRILESCU@student.utcluj.ro>
2020-05-16 22:06:14 +0300

    Ex2

--- c4-samples/src/main/java/aut/utcluj/isp/ex2/OnlineShop.java
+++ c4-samples/src/main/java/aut/utcluj/isp/ex2/OnlineShop.java
@@ -5,12 +5,29 @@ package aut.utcluj.isp.ex2;
  */
 public class OnlineShop {
     private String webAddress;
-
+    private String name;
+    private String city;
     public OnlineShop(String name, String city, String webAddress) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        this.webAddress=webAddress;
+        this.city=city;
+        this.name=name;
+        //throw new UnsupportedOperationException("Not supported yet.");
     }
+
+    public OnlineShop() {
+
+    }
+
+    @Override
+    public String toString() {
+        return "Shop:  "+ name+ " City: "+city+
+                " webAddress: " + webAddress ;
+    }
+
 
     public String getWebAddress() {
         return webAddress;
     }
+
+
 }
