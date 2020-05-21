b97cb94de4c3a194e9925910036672721251871e
diana.david <diana.david53@yahoo.com>
2020-05-22 02:35:33 +0300

    exercise 1 all test make

--- c4-samples/src/main/java/aut/utcluj/isp/ex1/Shop.java
+++ c4-samples/src/main/java/aut/utcluj/isp/ex1/Shop.java
@@ -8,12 +8,15 @@ public class Shop {
     private String city;
 
     public Shop(String name, String city) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        this.name=name;
+        this.city=city;
+
     }
 
     public Shop(String name) {
         this.city = "";
-        throw new UnsupportedOperationException("Not supported yet.");
+        this.name=name;
+       ;
     }
 
     public String getName() {
@@ -22,5 +25,14 @@ public class Shop {
 
     public String getCity() {
         return city;
+    }
+    public String toString(){
+        return  String.format("Shop: %s City: %s",getName(), getCity());
+    }
+    public boolean equals(Object o){
+        if(this==o) return  true;
+        if (o==null || getClass()!=o.getClass()) return false;
+        Shop shop=(Shop) o;
+        return true;
     }
 }
